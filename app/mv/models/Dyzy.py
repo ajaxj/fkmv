293a4a76021fa4357277471b6392942df04bfaaf
ajaxj <ajaxj@qq.com>
2013-07-08 15:46:25 +0800

    添加了 static 下面 bootstrap,js lib

---
+++ app/mv/models/Dyzy.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+from app.mv.extensions import db
+
+__author__ = 'Administrator'
+
+
+class DyzyMovie(db.Model):
+    pass
---
+++ app/mv/models/m3444.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+from app.mv.extensions import db
+
+__author__ = 'Administrator'
+
+
+class M3444Movie(db.Model):
+    pass
